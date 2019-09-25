493cb23dd463e467bac28ca19aa0c1edeccab663
Michael McBride <michaelxmcbride@icloud.com>
2019-09-25 15:13:46 -0400

    Add JFrog environment configuration for Zsh

---
+++ config/zsh/zshenv.d/jfrog.zsh
@@ -0,0 +1,7 @@
+# Environment variables for JFrog CLI.
+#
+# Links:
+# - https://www.jfrog.com/confluence/display/CLI/JFrog+CLI
+
+# Set the location of the JFrog CLI home directory.
+export JFROG_CLI_HOME_DIR="${XDG_DATA_HOME}/jfrog"
