5540b2cce386b59946f2fdec3ce1709d209602ae
Michael McBride <michaelxmcbride@icloud.com>
2019-10-25 11:51:02 -0400

    Add Zsh environment configuration for minikube

---
+++ config/zsh/zshenv.d/minikube.zsh
@@ -0,0 +1,8 @@
+# Environment variables for minikube.
+#
+# Links:
+# - https://minikube.sigs.k8s.io/docs/reference/environment_variables/
+
+# Set the location of the .minikube directory used for state and configuration
+# data.
+export MINIKUBE_HOME="${XDG_DATA_HOME}"
