52a780d874d4cb30bb21d399f6aa71320b542d41
Michael McBride <michaelxmcbride@icloud.com>
2019-12-17 12:30:19 -0500

    Add Zsh environment configuration for saml2aws

---
+++ config/zsh/zshenv.d/saml2aws.zsh
@@ -0,0 +1,7 @@
+# Zsh environment configuration for saml2aws.
+#
+# Links:
+# - https://github.com/Versent/saml2aws
+
+# Set the location of the saml2aws configuration file.
+export SAML2AWS_CONFIGFILE="${XDG_CONFIG_HOME}/saml2aws/config"
