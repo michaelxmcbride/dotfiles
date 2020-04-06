c496b8da0949fff0c194f060c6a7425766530177
Michael McBride <michaelxmcbride@icloud.com>
2020-04-06 13:53:52 -0400

    Add Zsh environment configuration for node-gyp

---
+++ config/zsh/zshenv.d/node-gyp.zsh
@@ -0,0 +1,8 @@
+# Zsh environment configuration for node-gyp.
+#
+# Links:
+# - https://github.com/nodejs/node-gyp
+# - https://github.com/nodejs/node-gyp#configuration
+
+# Set the path of the SDK download directory.
+export npm_config_devdir="${XDG_CACHE_HOME}/node-gyp"
