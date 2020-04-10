4b5f31db5cb162812c184470cfd70954139b5982
Michael McBride <michaelxmcbride@icloud.com>
2020-04-10 19:59:24 -0400

    Add Zsh environment configuration for IPython

---
+++ config/zsh/zshenv.d/ipython.zsh
@@ -0,0 +1,9 @@
+# Zsh environment configuration for IPython.
+#
+# Links:
+# - https://ipython.org/
+# - https://ipython.org/ipython-doc/stable/config/intro.html#the-ipython-directory
+
+# Set the path of the directory where IPython stores its files for
+# configuration, command history, and extensions.
+export IPYTHONDIR="${XDG_DATA_HOME}/ipython"
