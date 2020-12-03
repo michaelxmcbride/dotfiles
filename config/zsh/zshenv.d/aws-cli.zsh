ff6a2eda30f6de1b84350d647c0a44c84db7779e
Michael McBride <michaelxmcbride@icloud.com>
2020-12-02 20:41:14 -0500

    Update Zsh environment configuration for AWS

---
+++ config/zsh/zshenv.d/aws-cli.zsh
@@ -0,0 +1,8 @@
+# Zsh environment configuration for AWS Command Line Interface (CLI).
+#
+# Links:
+# - https://aws.amazon.com/cli/
+# - https://github.com/aws/aws-cli/commit/10887f260e4dfd2e9d495fb52db37287f35a491c
+
+# Set the location of the AWS CLI history file.
+export AWS_CLI_HISTORY_FILE="${XDG_DATA_HOME}/aws-cli/history"
--- config/zsh/zshenv.d/aws.zsh
+++ config/zsh/zshenv.d/aws.zsh
@@ -1,2 +1,12 @@
+# Zsh environment configuration for various AWS tooling (e.g. AWS CLI, AWS SDKs,
+# et cetera).
+#
+# Links:
+# - https://docs.aws.amazon.com/credref/latest/refdocs/overview.html
+# - https://docs.aws.amazon.com/credref/latest/refdocs/environment-variables.html
+
+# Set the location of the shared configuration file for various AWS tooling.
 export AWS_CONFIG_FILE="${XDG_CONFIG_HOME}/aws/config"
-export AWS_SHARED_CREDENTIALS_FILE="${XDG_CONFIG_HOME}/aws/credentials"
+
+# Set the location of the shared credentials file for various AWS tooling.
+export AWS_SHARED_CREDENTIALS_FILE="${XDG_DATA_HOME}/aws/credentials"
