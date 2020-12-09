3e81cda570bf01f39d80064495e6f2fa9540a844
Michael McBride <michaelxmcbride@icloud.com>
2020-12-09 03:41:29 -0500

    Add Zsh environment configuration for Terraform

---
+++ config/zsh/zshenv.d/terraform.zsh
@@ -0,0 +1,8 @@
+# Zsh environment configuration for Terraform.
+#
+# Links:
+# - https://www.terraform.io/
+# - https://www.terraform.io/docs/commands/environment-variables.html
+
+# The location of the Terraform CLI configuration file.
+export TF_CLI_CONFIG_FILE="${XDG_CONFIG_HOME}/terraform/terraformrc"
