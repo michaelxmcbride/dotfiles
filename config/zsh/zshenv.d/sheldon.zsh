223de3a83ce374521c086dbc183677cc712d1e4d
Michael McBride <michaelxmcbride@icloud.com>
2020-12-09 05:47:00 -0500

    Add Zsh environment configuration for Sheldon

---
+++ config/zsh/zshenv.d/sheldon.zsh
@@ -0,0 +1,10 @@
+# Zsh environment configuration for Sheldon.
+#
+# Links:
+# - https://rossmacarthur.github.io/sheldon/
+
+# The configuration directory.
+export SHELDON_CONFIG_DIR="${XDG_CONFIG_HOME}/sheldon"
+
+# The data directory.
+export SHELDON_DATA_DIR="${XDG_DATA_HOME}/sheldon"
